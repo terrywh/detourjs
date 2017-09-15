4f4862e202a4678959f65e319bf9342ba09115c2
terrywh <terry.wuhao@gmail.com>
2017-09-15 12:05:07 +0800

    分离 config 优化 master 进程控制；新增 nginx 多进程反响代理使用说明；

--- bin/detour.js
+++ bin/detour.js
@@ -1,21 +1,32 @@
+#!/usr/bin/env node
 "use strict";
 
-const cp = require("child_process"),
+const cluster = require("cluster"),
 	os = require("os"),
 	argv = require('minimist')(process.argv.slice(2));
 
-let port = parseInt(argv.port) || 60200;
-for(let i=0; i<os.cpus().length/4; ++i) {
-	fork(i);
-}
-function fork(i) {
-	cp.fork(__dirname + "/../lib/server/", {
-		env: {
+let port = parseInt(argv.port) || 60200,
+	proc = parseInt(argv.process) || os.cpus().length/2;
+
+if(cluster.isMaster) {
+	process.title = "detourjs (master)";
+
+	function fork(i) {
+		cluster.fork({
 			DETOUR_SERVER: "http://" 
 				+ (argv.bind || "127.0.0.1") 
 				+ ":" + (port + i + 1),
-		},
-	}).on("exit", function() {
-		setTimeout(fork, 2000 + Math.random() * 3000, i);
-	});	
+		}).on("exit", function() {
+			setTimeout(fork, 2000 + Math.random() * 3000, i);
+		});	
+	}
+	for(let i=0; i<proc; ++i) {
+		fork(i);
+	}
+}else{
+	let config = require("../lib/config");
+	process.title = "detourjs (server:" + config.server.port + ")";
+	require("../lib/server");
 }
+
+
